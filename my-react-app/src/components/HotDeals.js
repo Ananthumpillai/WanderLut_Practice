0dcbe6b049f3103f5e8943024d0fcf54b59dab77
ananthu-wq <64903554+ananthu-wq@users.noreply.github.com>
2023-04-27 22:12:46 +0530

    sidebar pending

--- my-react-app/src/components/HotDeals.js
+++ my-react-app/src/components/HotDeals.js
@@ -3,6 +3,10 @@ import axios from 'axios'
 import { TextField, Button } from '@mui/material';
 
 
+import { TabView, TabPanel } from 'primereact/tabview';
+
+import { Sidebar } from 'primereact/sidebar';
+
 
 
 const url = "http://localhost:1050/"
@@ -16,6 +20,7 @@ export default function HotDeals() {
         successMessage: "",
         errorMessage: ""
     })
+    const [sidebar, updateSidebar] = useState(false)
 
     useEffect(() => {
         fetchData()
@@ -32,61 +37,75 @@ export default function HotDeals() {
     }
     console.log(data);
     return (
-        <div className="container-fluid">
-
-            {data.map((singlePackage, index) => {
 
 
-                return <div className="card m-5">
-                    <div className="card-body p-5">
-                        <div className="row ">
-                            <div className="col-md-4">
+        <div>
+             {/* <Sidebar visible={sidebar} position="right" onHide={() => updateSidebar(false)}> 
+                <TabView>
+                    <TabPanel header="Header I">
+                        
+                    </TabPanel>
+                </TabView>
+            </Sidebar> */}
 
-                                <img className="package-image" src={process.env.PUBLIC_URL +singlePackage.imageUrl} alt="image"></img>
-                            </div>
-                            <div className="col-md-5">
+            <div className="container-fluid">
+
+                {data.map((singlePackage, index) => {
 
 
-                                <div className="featured-text text-lg-left">
-                                <h4>{singlePackage.name}</h4>
-                                <div className="badge bg-info">{singlePackage.noOfNights}<em> Nights</em></div>
-                                {singlePackage.discount ? <div className="discount text-danger">{singlePackage.discount}% Instant Discount</div> : null}
-                                <p className="text-dark mb-0">{singlePackage.details.about}</p>
+                    return <div className="card m-5">
+                        <div className="card-body p-5">
+                            <div className="row ">
+                                <div className="col-md-4">
+
+                                    <img className="package-image" src={process.env.PUBLIC_URL + singlePackage.imageUrl} alt="image"></img>
                                 </div>
-                                <br />
-                            </div>
-                            <div className="col-md-3">
-                            <h4>Prices Starting From:</h4>
-                            <div className="text-center text-success"><h6>₹ {singlePackage.chargesPerPerson}</h6></div><br /><br />
-                           <div>
-                             <Button
-                                               
-                                                fullWidth
-                                                variant="contained"
-                                                color="primary"
-                                              
-                                            >
-                                               View Details
-                                           </Button></div> 
-                            {/* <div><button className="btn btn-primary book" onClick={() => this.getItinerary(singlePackage)}>View Details</button></div><br /> */}
-                            {/* <div><br/><button className="btn btn-primary book" onClick={() => this.openBooking(singlePackage)}>Book </button>  </div> */}
-                           <div><br/>
-                            <Button
-                                               
-                                               fullWidth
-                                               variant="contained"
-                                               color="primary"
-                                             
-                                           >
-                                            Book
-                                          </Button> 
-                                          </div>
+                                <div className="col-md-5">
 
+
+                                    <div className="featured-text text-lg-left">
+                                        <h4>{singlePackage.name}</h4>
+                                        <div className="badge bg-info">{singlePackage.noOfNights}<em> Nights</em></div>
+                                        {singlePackage.discount ? <div className="discount text-danger">{singlePackage.discount}% Instant Discount</div> : null}
+                                        <p className="text-dark mb-0">{singlePackage.details.about}</p>
+                                    </div>
+                                    <br />
+                                </div>
+                                <div className="col-md-3">
+                                    <h4>Prices Starting From:</h4>
+                                    <div className="text-center text-success"><h6>₹ {singlePackage.chargesPerPerson}</h6></div><br /><br />
+                                    <div>
+                                        <Button
+
+                                            fullWidth
+                                            variant="contained"
+                                            color="primary"
+                                            onClick={() => updateSidebar(true)}
+                                        >
+                                            View Details
+
+                                        </Button></div>
+                                    {/* <div><button className="btn btn-primary book" onClick={() => this.getItinerary(singlePackage)}>View Details</button></div><br /> */}
+                                    {/* <div><br/><button className="btn btn-primary book" onClick={() => this.openBooking(singlePackage)}>Book </button>  </div> */}
+                                    <div><br />
+                                        <Button
+
+                                            fullWidth
+                                            variant="contained"
+                                            color="primary"
+
+                                        >
+                                            Book
+                                        </Button>
+                                    </div>
+
+                                </div>
                             </div>
                         </div>
                     </div>
-                </div>
-            })}
+                })}
+            </div>
+
         </div>
     )
 }
